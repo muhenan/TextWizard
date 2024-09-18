39e65d93e2cbd76a0d20203ef2b30af632816efc
Henan Mu <1149423577@qq.com>
2024-09-18 01:21:34 -0400

    CORSMiddleware

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,12 +2,32 @@ package routes
 
 import (
 	"TextWizard/internal/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 设置CORS头
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// 处理 OPTIONS 请求
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 // InitializeRoutes sets up all the routes in one place
 func InitializeRoutes(router *gin.Engine) {
+	router.Use(CORSMiddleware())
+
 	router.GET("/test", handlers.Test)
 	router.POST("/summarize", handlers.SummarizeTextHandler)
 }
